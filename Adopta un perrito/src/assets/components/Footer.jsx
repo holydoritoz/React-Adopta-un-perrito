a6c08a9b8d9c319cc685b9e6e24615fe3f401cd3
Dario Urdaneta <dariourdaneta@gmail.com>
2023-09-26 21:53:16 -0300

    adding url

--- Adopta un perrito/src/assets/components/Footer.jsx
+++ Adopta un perrito/src/assets/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     En nuestra galería de imágenes de perros en adopción, celebramos la oportunidad de unir a los corazones
     de las familias con sus fieles amigos de cuatro patas. 
     </p>
-    <p>React I - Desafio Latam / Dario Urdaneta <a href="">Github</a></p>
+    <p>React I - Desafio Latam / Dario Urdaneta <a href="https://github.com/holydoritoz/React-Adopta-un-perrito">Github</a></p>
     </>
   )
 }
